6c682a4c909133098674bb3f3721cc7d9ceecbb3
eemeli <eemeliruoh@gmail.com>
2025-04-13 20:23:30 +0300

    remove favicon, add meta and fix key thingy

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,98 +4,98 @@ import { skills } from "~/utils/knowledge";
 import type { Skill } from "~/utils/types";
 import type { Route } from "./+types/home";
 import { contacts } from "~/utils/contacts";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Link from "~/components/Link";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "eemeli ruohomäki" },
+		{ name: "description", content: "eemeli's website" },
+	];
 }
 
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-4xl">eemeli ruohomäki</h1>
-      <section className="flex flex-col gap-1">
-        <p>a computer science student who enjoys making modest websites.</p>
-        <p>
-          based in Tampere, Finland, where it's currently <Time />.
-        </p>
-      </section>
-      <section className="flex flex-col gap-4">
-        <h2>projects</h2>
-        <ProjectsCarousel projects={projects} />
-      </section>
-      <section className="flex flex-col gap-4">
-        <h2>knowledge</h2>
-        <Knowledge knowledge={skills} />
-      </section>
-      <section className="flex flex-col gap-4">
-        <h2>contact</h2>
-        <div className="flex gap-2 flex-wrap">
-          {contacts.map((contact, index) => (
-            <>
-              <Link href={contact.link} key={index} target="_blank">
-                {contact.title}
-              </Link>
-              {index < contacts.length - 1 && (
-                <span className="text-neutral-700">|</span>
-              )}
-            </>
-          ))}
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-8">
+			<h1 className="text-4xl">eemeli ruohomäki</h1>
+			<section className="flex flex-col gap-1">
+				<p>a computer science student who enjoys making modest websites.</p>
+				<p>
+					based in Tampere, Finland, where it's currently <Time />.
+				</p>
+			</section>
+			<section className="flex flex-col gap-4">
+				<h2>projects</h2>
+				<ProjectsCarousel projects={projects} />
+			</section>
+			<section className="flex flex-col gap-4">
+				<h2>knowledge</h2>
+				<Knowledge knowledge={skills} />
+			</section>
+			<section className="flex flex-col gap-4">
+				<h2>contact</h2>
+				<div className="flex gap-2 flex-wrap">
+					{contacts.map((contact, index) => (
+						<Fragment key={index}>
+							<Link href={contact.link} key={index} target="_blank">
+								{contact.title}
+							</Link>
+							{index < contacts.length - 1 && (
+								<span className="text-neutral-700">|</span>
+							)}
+						</Fragment>
+					))}
+				</div>
+			</section>
+		</div>
+	);
 }
 
 function Section({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col gap-4">
-      <h2>Section</h2>
-      <div className="flex flex-col gap-4">{children}</div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4">
+			<h2>Section</h2>
+			<div className="flex flex-col gap-4">{children}</div>
+		</div>
+	);
 }
 
 function Knowledge({ knowledge }: { knowledge: Skill[] }) {
-  return (
-    <div className="flex flex-wrap border border-neutral-700">
-      {knowledge.map((skill, index) => (
-        <div
-          key={index}
-          className="p-4 flex-1 min-w-52 border-r border-b border-neutral-700 last:border-r-0"
-        >
-          <h3>{skill.category}</h3>
-          <ul className="list-disc pl-4">
-            {skill.items.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap border border-neutral-700">
+			{knowledge.map((skill, index) => (
+				<div
+					key={index}
+					className="p-4 flex-1 min-w-52 border-r border-b border-neutral-700 last:border-r-0"
+				>
+					<h3>{skill.category}</h3>
+					<ul className="list-disc pl-4">
+						{skill.items.map((item, index) => (
+							<li key={index}>{item}</li>
+						))}
+					</ul>
+				</div>
+			))}
+		</div>
+	);
 }
 
 function Time() {
-  const [time, setTime] = useState(new Date());
+	const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    // Update time every minute (60000ms)
-    const intervalId = setInterval(() => setTime(new Date()), 60000);
+	useEffect(() => {
+		// Update time every minute (60000ms)
+		const intervalId = setInterval(() => setTime(new Date()), 60000);
 
-    // Clean up the interval when component unmounts
-    return () => clearInterval(intervalId);
-  }, []);
+		// Clean up the interval when component unmounts
+		return () => clearInterval(intervalId);
+	}, []);
 
-  return (
-    <span>
-      {time.getHours().toString().padStart(2, "0")}
-      <span className="blink">:</span>
-      {time.getMinutes().toString().padStart(2, "0")}
-    </span>
-  );
+	return (
+		<span>
+			{time.getHours().toString().padStart(2, "0")}
+			<span className="blink">:</span>
+			{time.getMinutes().toString().padStart(2, "0")}
+		</span>
+	);
 }
