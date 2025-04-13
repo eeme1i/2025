22495a41734180ee154c1304e0e7ac11444f3112
eemeli <eemeliruoh@gmail.com>
2025-04-13 20:14:12 +0300

    remove iconoir

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,21 +1,18 @@
-import { ArrowUpRight } from "iconoir-react/regular";
-
 type LinkProps = {
-  href: string;
-  children: React.ReactNode;
-  target?: string;
+	href: string;
+	children: React.ReactNode;
+	target?: string;
 };
 
 export default function Link({ href, children, target }: LinkProps) {
-  return (
-    <a
-      href={href}
-      target={target}
-      rel="noopener noreferrer"
-      className="text-blue-400 hover:underline flex items-start w-max"
-    >
-      <span>{children}</span>
-      {target === "_blank" && <ArrowUpRight height={14} />}
-    </a>
-  );
+	return (
+		<a
+			href={href}
+			target={target}
+			rel="noopener noreferrer"
+			className="text-blue-400 hover:underline flex items-start w-max"
+		>
+			<span>{children}</span>
+		</a>
+	);
 }
